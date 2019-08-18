fb9561c2f8bc5fc15a5a0f379a8250ce766a2cc4
Tatyana Derbysheva <tatyderb@gmail.com>
2019-08-18 15:35:33 +0300

    add fake auth data to repository

---
+++ auth_data.py
@@ -0,0 +1,14 @@
+"""
+Authentication data for Stepik + get token
+"""
+
+
+API_HOST = 'https://stepik.org'
+
+# Enter parameters below:
+# 1. Get your keys at https://stepik.org/oauth2/applications/
+# (client type = confidential, authorization grant type = client credentials)
+CLIENT_ID = "..."
+CLIENT_SECRET = "..."
+
+course_id = 55690
