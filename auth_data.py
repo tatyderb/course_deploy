25603370c4c740f521da0b1ca98ce3631e46c5aa
Tatyana Derbysheva <tatyderb@gmail.com>
2019-08-28 17:40:01 +0300

    auth file

---
+++ auth_data.py
@@ -0,0 +1,17 @@
+"""
+Authentication data for Stepik + get token
+"""
+
+
+API_HOST = 'https://stepik.org'
+
+# Enter parameters below:
+# 1. Get your keys at https://stepik.org/oauth2/applications/
+# (client type = confidential, authorization grant type = client credentials)
+#client_id = "..."
+#client_secret = "..."
+#course_id = 1
+CLIENT_ID = "..."
+CLIENT_SECRET = "...r"
+
+course_id = 55690
