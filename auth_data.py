8d072ffc0fbbebcf501fe5d22cfdf8b038108952
Tako-San <dxe13@mail.ru>
2020-07-11 19:07:10 +0400

    Small changes

--- auth_data.py
+++ auth_data.py
@@ -9,9 +9,9 @@ API_HOST = 'https://stepik.org'
 # Enter parameters below:
 # 1. Get your keys at https://stepik.org/oauth2/applications/
 # (client type = confidential, authorization grant type = client credentials)
-#client_id = "..."
-#client_secret = "..."
-#course_id = 1
+# client_id = "..."
+# client_secret = "..."
+# course_id = 1
 
 CLIENT_ID = "..."
 CLIENT_SECRET = "..."
