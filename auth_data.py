7a1f2ed96f4ea1fb94b4a2a1da85fe806bce5b9d
Tako-San <dxe13@mail.ru>
2020-07-12 20:14:36 +0400

    Standard authorisation data massage improved

--- auth_data.py
+++ auth_data.py
@@ -20,8 +20,9 @@ CLIENT_SECRET = "..."
 def auth_check():
     if CLIENT_ID == '...' or CLIENT_SECRET == '...':
         print('Wrong client credentials.')
-        print('Go to https://stepik.org/oauth2/applications/, get authorisation data and write into auth.py file.')
-        print('Client type = confidential, authorization grant type = client credentials.')
+        print('Go to https://stepik.org/oauth2/applications/, get authorisation data and write into auth_data.py file:')
+        print("Change this variables: CLIENT_ID = '...', CLIENT_SECRET = '...'")
+        print('(For stepik: client type = confidential, authorization grant type = client credentials.)')
         sys.exit()
 
 
